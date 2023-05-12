4c7c4e383bdcdeabd8f787ef9960b94406f1cb3b
Ing. Mauricio Carreño <122670509+ingmcarreno@users.noreply.github.com>
2023-05-12 09:35:56 -0400

    Update __manifest__.py

--- fact_send/__manifest__.py
+++ fact_send/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "GURU send Invoice",
+    'name': "Kraken send Invoice",
 
     'summary': """
         Electronic Online invoicing by Bolivia's statements""",
@@ -9,12 +9,8 @@
         Online invoicing
     """,
 
-    'author': "MF Consulting",
+    'author': "MACS Consulting",
     'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'account',
     'version': '0.1',
 
